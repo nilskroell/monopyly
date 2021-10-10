ba836e8ecc4116643cd0a1c7e4c3546c0d8d16f4
Nils Kroell <nils.kroell@ants.rwth-aachen.de>
2021-10-10 11:40:08 +0200

    #6: Add actioncard.

--- actioncard.py
+++ actioncard.py
@@ -1,12 +1,31 @@
-class ActionCard():
+from playfields import NonProperty
 
-    def __init__(self) -> None:
-        
-        # move forward
-        # move backward
-        # teleport
 
-        # pay money
-        # get money
+class ActionCard(NonProperty):
 
-        # pay_per_house
+    def __init__(self,
+                 position: int,
+                 target_position=None,
+                 n_steps_forward: int = 0,
+                 n_steps_backward: int = 0,
+                 money_to_pay: int = 0,
+                 money_to_get: int = 0,
+                 money_to_pay_per_house: int = 0
+                 ) -> None:
+        super().__init__(position)
+
+        for var in (position,
+                    target_position,
+                    n_steps_forward,
+                    n_steps_backward,
+                    money_to_pay,
+                    money_to_get,
+                    money_to_pay_per_house):
+            assert (isinstance(var, int) and var > 0)
+
+        self.target_position = target_position
+        self.n_steps_forward = n_steps_forward
+        self.n_steps_backward = n_steps_backward
+        self.money_to_pay = money_to_pay
+        self.money_to_get = money_to_get
+        self.money_to_pay_per_house = money_to_pay_per_house
