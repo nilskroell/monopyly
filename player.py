b403b00552f1da184d5ddcb11a5cae49e4ddc262
Nils Kroell <nils.kroell@ants.rwth-aachen.de>
2021-10-10 14:22:33 +0200

    Automatic ID generation for players.

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 class Player():
 
-    def __init__(self, id: int, start_balance: int = 1500, position: int = 0) -> None:
-        self.id = id
+    def __init__(self, start_balance: int = 1500, position: int = 0) -> None:
+        self.id = None
         self.balance = start_balance
         self.position = position
         self.alive = True
